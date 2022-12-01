06084ac7e6bc7dad7694a336a20000208af9ba84
Nitin1692 <84765879+Nitin1692@users.noreply.github.com>
2022-12-01 17:19:34 +0530

    Made chamges for password length

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const lengthSlider = document.querySelector(".pass-length input");
 const updateSlider = () => {
     document.querySelector(".pass-length span").innerText = lengthSlider.value;
 }
+updateSlider();
 
 lengthSlider.addEventListener("input", updateSlider)
