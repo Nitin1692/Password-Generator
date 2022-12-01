96f7517e07ca61683083cffc6731ad30e6e99d1d
Nitin1692 <84765879+Nitin1692@users.noreply.github.com>
2022-12-01 19:19:33 +0530

    Made changes for password indicator

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const lengthSlider = document.querySelector(".pass-length input"),
 options = document.querySelectorAll(".option input"),
 passwordInput = document.querySelector(".input-box input"),
+passIndicator = document.querySelector(".pass-indicator"),
 generateBtn = document.querySelector(".generate-btn");
 
 const characters = {
@@ -43,9 +44,14 @@ const generatePassword = () => {
     
 }
 
+const updatePassIndicator = () => {
+    passIndicator.id = lengthSlider.value <= 8 ? "weak" : lengthSlider.value <= 16 ? "medium" : "strong";
+}
+
 const updateSlider = () => {
     document.querySelector(".pass-length span").innerText = lengthSlider.value;
     generatePassword();
+    updatePassIndicator();
 }
 updateSlider();
 
